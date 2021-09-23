c0dbb8b2cb481e2856c4ac1fbb4d7d5dff60f4b5
iiskhrn139 <193040139.iis@mail.unpas.ac.id>
2021-09-23 21:58:46 +0700

    coba php

---
+++ coba.php
@@ -0,0 +1,26 @@
+<?php
+class Coba {
+    public $a;//property
+
+    //method 
+    public function b(){
+
+    }
+
+    $a= new Coba();
+    $b = new Coba();
+    $c = new Coba();
+    var_dump ($a);
+}
+
+
+
+
+
+
+
+
+
+
+
+?>
