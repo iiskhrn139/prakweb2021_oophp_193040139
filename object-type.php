705c96b93966cc3872f3d9f32469eea4c9539b75
iiskhrn139 <193040139.iis@mail.unpas.ac.id>
2021-09-24 13:15:58 +0700

    object-type.php

--- object-type.php
+++ object-type.php
@@ -10,7 +10,7 @@ class Produk {
             $penerbit, 
             $harga ;
 
-    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga) = 0 {
+    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0) {
         $this->judul = $judul;
         $this->penulis = $penulis;
         $this->penerbit = $penerbit;
